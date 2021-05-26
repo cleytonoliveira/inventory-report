3feb26b6831bb22da7589ac5e7ffffe4c4ba26ab
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-05-26 15:55:05 -0300

    feat(inventory): xml implementation to class inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,33 +1,49 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
+
+def reporter_csv(file):
+    reporter = []
+    reporter_csv = csv.DictReader(file)
+    for row in reporter_csv:
+        reporter.append(row)
+    return reporter
+
+
+def reporter_json(file):
+    content = file.read()
+    reporter = json.loads(content)
+    return reporter
+
+
+def reporter_xml(file):
+    reporter = []
+    tree = ET.parse(file)
+    root = tree.getroot()
+    all_records = root.findall('record')
+    for item in all_records:
+        inventory = {element.tag: element.text for element in item}
+        reporter.append(inventory)
+    return reporter
 
 
 class Inventory:
     @classmethod
     def import_data(self, pathfile, type_report):
-        if pathfile[len(pathfile) - 1] == 'v':
-            with open(pathfile) as file:
-                reporter_csv = csv.DictReader(file)
-                reporter = []
-                for row in reporter_csv:
-                    reporter.append(row)
+        with open(pathfile) as file:
+            if pathfile.endswith('csv'):
+                reporter = reporter_csv(file)
 
-                if type_report == 'simples':
-                    return SimpleReport.generate(reporter)
-                else:
-                    return CompleteReport.generate(reporter)
+            elif pathfile.endswith('json'):
+                reporter = reporter_json(file)
 
-        else:
-            with open(pathfile) as file:
-                content = file.read()
-                reporter_json = json.loads(content)
+            else:
+                reporter = reporter_xml(file)
 
-                if type_report == 'simples':
-                    return SimpleReport.generate(reporter_json)
-                else:
-                    return CompleteReport.generate(reporter_json)
-
-
-# print(Inventory.import_data('/home/cleyton/Documents/Projects/computer-science/sd-06-inventory-report/inventory_report/data/inventory.json', 'simples'))
+            if type_report == 'simples':
+                return SimpleReport.generate(reporter)
+            else:
+                return CompleteReport.generate(reporter)
